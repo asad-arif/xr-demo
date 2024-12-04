1704a8cf7a4dd54453807d235d03d476282ed188
Asad <asadofficial01@gmail.com>
2024-12-04 15:30:18 +0500

    support check added in ar

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ const store = createXRStore()
 
 function App() {
   const [red, setRed] = useState(false)
+  const [arSupported, setArSupported] = useState(true)
+
+  // Check if WebXR is supported
+  if (!navigator.xr) {
+    setArSupported(false)
+  }
+
   return (
     <>
-      <button onClick={() => store.enterAR()}>Enter AR</button>
-      <Canvas>
-        <XR store={store}>
-          <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
-            <boxGeometry />
-            <meshBasicMaterial color={red ? 'red' : 'blue'} />
-          </mesh>
-        </XR>
-      </Canvas>
+      {!arSupported ? (
+        <div>
+          <p>AR is not supported on your device or browser.</p>
+        </div>
+      ) : (
+        <>
+          <button onClick={() => store.enterAR()}>Enter AR</button>
+          <Canvas>
+            <XR store={store}>
+              <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+                <boxGeometry />
+                <meshBasicMaterial color={red ? 'red' : 'blue'} />
+              </mesh>
+            </XR>
+          </Canvas>
+        </>
+      )}
     </>
   )
 }
